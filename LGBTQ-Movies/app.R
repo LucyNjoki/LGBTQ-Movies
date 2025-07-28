3d72eb2f99276b9e570ee3fe93d8bc0bb9acb5b8
LucyNjokiNjuki <njoki.njuki@ndorms.ox.ac.uk>
2025-07-28 12:57:36 +0100

    Update app.R

--- LGBTQ-Movies/app.R
+++ LGBTQ-Movies/app.R
@@ -1,3 +1,4 @@
+# Load Libraries
 library(shiny)
 library(shinydashboard)
 library(DT)
@@ -6,180 +7,230 @@ library(lubridate)
 library(rio)
 library(here)
 library(gglgbtq)
+library(tidytext)
+library(wordcloud)
+library(RColorBrewer)
+library(bslib)
+library(plotly)
 
+# UI
 ui <- fluidPage(
-  titlePanel("LGBT+ Movies Explorer 🌈"),
+  theme = bs_theme(bootswatch = "superhero"), 
+
+  # Logo at the top
+  titlePanel(
+    div(
+      img(src = "lgbtq_movies_hex_final.png", height = "50px", style = "margin-right:15px;"),
+      "LGBT+ Movies Explorer",
+      style = "display: flex; align-items: center;"
+    )
+  ),
+
+  # Rainbow banner
   div(
-    style = "height: 200px; width: 100%; background: linear-gradient(to right, #E40303, #FF8C00, #FFED00, #008026, #004DFF, #8B00FF);"
+    style = "height: 25px; width: 100%; background: linear-gradient(to right, #E40303, #FF8C00, #FFED00, #008026, #004DFF, #8B00FF);"
   ),
   sidebarLayout(
     sidebarPanel(
-      selectInput("keyword", "Select Keyword:", 
-                  choices = c("All", "LGBT", "Gay", "Lesbian", "Transgender", "bisexual", 
-                              "Intersex", "Queer", "GendeQqueer", "Non-binary", "gender", "asexual")),
-      sliderInput("yearRange", "Release Year:", 
-                  min = 1950, max = 2025, value = c(2000, 2025), sep = ""),
+      selectInput("keyword", "Select Keyword:",
+        choices = c(
+          "All", "LGBT", "Gay", "Lesbian", "Transgender", "Bisexual",
+          "Intersex", "Queer", "Genderqueer", "Non-binary", "Gender", "Asexual"
+        )
+      ),
+      sliderInput("yearRange", "Release Year:",
+        min = 1950, max = 2025, value = c(2000, 2025), sep = ""
+      ),
       checkboxInput("showAdult", "Include Adult Movies", value = FALSE),
       checkboxInput("topRated", "Show Only Top Rated (8+)", value = FALSE)
     ),
-    
     mainPanel(
       tabsetPanel(
-        tabPanel("Overview", 
-                 valueBoxOutput("movieCount"),
-                 valueBoxOutput("avgRating"),
-                 valueBoxOutput("popularity")),
-        
-        tabPanel("Trends", 
-                 plotOutput("releaseTrend"),
-                 plotOutput("ratingTrend")),
-        
-        tabPanel("Genre Breakdown", 
-                 plotOutput("genrePlot")),
-        
-        tabPanel("Top Movies", 
-                 dataTableOutput("topMovies")),
-        
-        tabPanel("Language Insights", 
-                 plotOutput("langPlot"))
+        tabPanel(
+          "Overview",
+          fluidRow(
+            column(
+              width = 4,
+              style = "padding-right:10px;", 
+              valueBoxOutput("movieCount")),
+            column(
+              width = 4, 
+              style = "padding-right:10px;", 
+              valueBoxOutput("avgRating")),
+            column(
+              width = 4, 
+              valueBoxOutput("popularity"))
+          ),
+          hr(),
+          h4("Common Themes in Movie Descriptions"),
+          plotOutput("wordcloud", height = "450px", width = "600px")
+        ),
+        tabPanel(
+          "Trends",
+          plotOutput("releaseTrend"),
+          plotOutput("ratingTrend")
+        ),
+        tabPanel(
+          "Genre Breakdown",
+          plotlyOutput("genrePlot")
+        ),
+        tabPanel(
+          "Top Movies",
+          dataTableOutput("topMovies")
+        ),
+        tabPanel(
+          "Language Insights",
+          plotOutput("langPlot")
+        )
       )
     )
   )
 )
 
-
+# Server
 server <- function(input, output, session) {
   filteredData <- reactive({
-    movies_data = import(here("Data", "movieDataCleaned.xlsx"))
-    df <- movies_data  # your loaded dataset
-    
-    # Filter by keyword (assuming keyword is a column or derived from a list)
+    movies_data <- import(here("Data", "movieDataCleaned.xlsx"))
+    df <- movies_data
+
     if (input$keyword != "All") {
-      df <- df %>% filter(str_detect(tolower(overview), tolower(input$keyword)))
+      df <- df %>%
+        filter(str_detect(tolower(overview), tolower(input$keyword)))
     }
-    
-    # Filter by year
-    df <- df %>% filter(lubridate::year(as.Date(release_date)) >= input$yearRange[1],
-                        lubridate::year(as.Date(release_date)) <= input$yearRange[2])
-    
-    # Filter adult
+
+    df <- df %>% filter(
+      lubridate::year(as.Date(release_date)) >= input$yearRange[1],
+      lubridate::year(as.Date(release_date)) <= input$yearRange[2]
+    )
+
     if (!input$showAdult) {
       df <- df %>% filter(adult == FALSE)
     }
-    
-    # Top rated
+
     if (input$topRated) {
       df <- df %>% filter(vote_average >= 8)
     }
-    
+
     df
   })
-  
-  # output$movieCount <- renderValueBox({
-  #   valueBox(nrow(filteredData()), "Movies", icon = icon("film"))
-  # })
-  # 
-  # output$avgRating <- renderValueBox({
-  #   valueBox(round(mean(filteredData()$vote_average, na.rm = TRUE), 2), "Avg Rating", icon = icon("star"))
-  # })
-  # 
-  # output$popularity <- renderValueBox({
-  #   valueBox(round(mean(filteredData()$popularity, na.rm = TRUE), 2), "Avg Popularity", icon = icon("fire"))
-  # })
-  
-  
+
   output$movieCount <- renderValueBox({
     valueBox(
-      nrow(filteredData()), 
-      "Movies", 
-      icon = icon("film"), 
-      color = "purple"
-    )
+      value = nrow(filteredData()),
+      subtitle = "Movies",
+      icon = icon("film"),
+      color = "purple",
+      width = 4
+    ) %>%
+      tagAppendAttributes(style = "color: white;") # white text on purple
   })
-  
+
   output$avgRating <- renderValueBox({
     valueBox(
-      round(mean(filteredData()$vote_average, na.rm = TRUE), 2), 
-      "Avg Rating", 
-      icon = icon("star"), 
-      color = "yellow"
-    )
+      value = round(mean(filteredData()$vote_average, na.rm = TRUE), 2),
+      subtitle = "Avg Rating",
+      icon = icon("star"),
+      color = "yellow",
+      width = 4
+    ) %>%
+      tagAppendAttributes(style = "color: black;") # black text on yellow
   })
-  
+
   output$popularity <- renderValueBox({
     valueBox(
-      round(mean(filteredData()$popularity, na.rm = TRUE), 2), 
-      "Avg Popularity", 
-      icon = icon("fire"), 
-      color = "red"
+      value = round(mean(filteredData()$popularity, na.rm = TRUE), 2),
+      subtitle = "Avg Popularity",
+      icon = icon("fire"),
+      color = "red",
+      width = 4
+    ) %>%
+      tagAppendAttributes(style = "color: white;") # white text on red
+  })
+
+
+  output$wordcloud <- renderPlot({
+    text_data <- filteredData() %>%
+      select(overview) %>%
+      unnest_tokens(word, overview) %>%
+      anti_join(stop_words) %>%
+      count(word, sort = TRUE)
+
+    wordcloud(
+      words = text_data$word, freq = text_data$n,
+      max.words = 100, colors = brewer.pal(8, "Dark2")
     )
   })
-  
-  
+
   output$releaseTrend <- renderPlot({
     filteredData() %>%
       mutate(year = lubridate::year(as.Date(release_date))) %>%
       count(year) %>%
       ggplot(aes(x = year, y = n)) +
-      geom_line() + geom_point() +
-      labs(title = "LGBT+ Movie Releases Over Time", 
-           x = "Year",
-           y = "Number of Movies") +
-      ggthemes::theme_stata()
+      geom_line(colour = "black", size = 0.8) +
+      geom_point(colour = "black", size = 1.2) +
+      labs(
+        title = "LGBT+ Movie Releases Over Time",
+        x = "Year",
+        y = "Number of Movies"
+      ) +
+      theme_minimal()
   })
-  
+
   output$ratingTrend <- renderPlot({
     filteredData() %>%
       mutate(year = lubridate::year(as.Date(release_date))) %>%
       group_by(year) %>%
       summarise(avg_rating = mean(vote_average, na.rm = TRUE)) %>%
       ggplot(aes(x = year, y = avg_rating)) +
-      geom_line(color = "#68228B") + 
-      geom_point(color = "#68228B") +
-      labs(title = "Average Rating by Year", 
-           x = "Year", 
-           y = "Avg Rating") +
+      geom_line(color = "#68228B", size = 0.8) +
+      geom_point(color = "#68228B", size = 1.2) +
+      labs(title = "Average Rating by Year", x = "Year", y = "Avg Rating") +
       ggthemes::theme_hc()
   })
-  
-  output$genrePlot <- renderPlot({
+
+  output$genrePlot <- renderPlotly({
     genre_data <- filteredData() %>%
-      # unnest(genre_ids) %>%   # assuming it's stored as a list/array
-      count(genre_ids_recoded)
-    
-    ggplot(genre_data, 
-           aes(x = fct_reorder(as.factor(genre_ids_recoded), n), 
-               y = n)) +
+      count(genre_ids_recoded) %>%
+      mutate(genre = fct_reorder(genre_ids_recoded, n))
+
+    p <- ggplot(genre_data, aes(x = genre, y = n)) +
       geom_col(fill = "darkgreen") +
       coord_flip() +
-      labs(title = "Most Common Genres", 
-           x = "Genre ID", 
-           y = "Count")
-      
+      labs(title = "Most Common Genres", x = "Genre", y = "Count") +
+      ggdark::dark_theme_dark()
+
+    ggplotly(p)
   })
-  
+
+
+
   output$langPlot <- renderPlot({
     filteredData() %>%
       count(original_language_recoded) %>%
       top_n(10, n) %>%
-      ggplot(aes(x = fct_reorder(original_language_recoded, n),
-                 y = n,
-                 fill = original_language_recoded)) +
+      ggplot(aes(
+        x = fct_reorder(original_language_recoded, n),
+        y = n,
+        fill = original_language_recoded
+      )) +
       geom_col() +
       coord_flip() +
-      labs(title = "Top Languages", 
-           x = "Language", 
-           y = "Movies") +
+      labs(
+        title = "Top 10 Languages",
+        x = "Language",
+        y = "Movies"
+      ) +
       scale_fill_manual(values = palette_lgbtq("progress")) +
       theme_lgbtq("progress", legend.position = "none")
-    
   })
-  
+
   output$topMovies <- renderDataTable({
     filteredData() %>%
       arrange(desc(vote_average)) %>%
-      select(title, vote_average, popularity, release_date)
+      select(title, vote_average, popularity, release_date) |>
+      distinct()
   })
 }
 
+# Run the App
 shinyApp(ui = ui, server = server)
